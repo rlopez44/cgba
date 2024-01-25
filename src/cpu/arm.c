72d8d2db0c2d7ae2cf42aa8f7b7c5ea308aa0290
Rodrigo Lopez <68488284+rlopez44@users.noreply.github.com>
2024-01-24 23:49:20 -0600

    Implement MULL and MLAL instructions

--- src/cpu/arm.c
+++ src/cpu/arm.c
@@ -685,24 +685,37 @@ static int msr_transfer(arm7tdmi *cpu, uint32_t inst)
 }
 
 /*
- * Calculate how many 8-bit multiplier array
- * cycles the MUL/MLA instruction required.
+ * Calculate how many 8-bit multiplier array cycles
+ * the MUL/MLA/MULL/MLAL instruction required.
  *
+ * MUL/MLA/SMULL/SMLAL
+ * --------------------
  * 1 if Rs[31:8] are all zero or all one
  * 2 if Rs[31:16] are all zero or all one
  * 3 if Rs[31:24] are all zero or all one
  * 4 otherwise
+ *
+ * UMULL/UMLAL
+ * -----------
+ * 1 if Rs[31:8] are all zero
+ * 2 if Rs[31:16] are all zero
+ * 3 if Rs[31:24] are all zero
+ * 4 otherwise
  */
-static int get_multiply_array_cycles(uint32_t rs)
+static int get_multiply_array_cycles(uint32_t rs, bool mul_long, bool signed_)
 {
     int num_clocks = 4;
     uint32_t work_val = rs;
     uint32_t ref_val = 0xffffffff;
+    bool match_all_ones = !mul_long || signed_;
     for (int i = 1; i < 4; ++i)
     {
         work_val >>= 8;
         ref_val >>= 8;
-        if (!work_val || work_val == ref_val)
+        bool all_zeros = !work_val;
+        bool all_ones  = work_val == ref_val;
+
+        if (all_zeros || (match_all_ones && all_ones))
         {
             num_clocks = i;
             break;
@@ -714,13 +727,8 @@ static int get_multiply_array_cycles(uint32_t rs)
 
 static int multiply(arm7tdmi *cpu, uint32_t inst)
 {
-    bool mul_long = (inst >> 23) & 1;
-    if (mul_long)
-    {
-        fputs("Error: MULL/MLAL instructions not yet implemented\n", stderr);
-        exit(1);
-    }
-
+    bool mul_long   = (inst >> 23) & 1;
+    bool signed_    = (inst >> 22) & 1; // always zero for MUL/MLA
     bool accumulate = (inst >> 21) & 1;
     bool set_conds  = (inst >> 20) & 1;
     int rd = (inst >> 16) & 0xf;
@@ -730,27 +738,60 @@ static int multiply(arm7tdmi *cpu, uint32_t inst)
 
     prefetch(cpu);
 
-    uint32_t rs_value = read_register(cpu, rs);
-    uint32_t result = read_register(cpu, rm) * rs_value;
-    if (accumulate)
-        result += read_register(cpu, rn);
+    // store as 64-bit so we can do both
+    // multiply and multiply long instructions
+    uint64_t rs_value = read_register(cpu, rs);
+    uint64_t rm_value = read_register(cpu, rm);
 
-    write_register(cpu, rd, result);
+    if (signed_)
+    {
+        uint64_t extension = (uint64_t)UINT32_MAX << 32;
+        if ((rs_value >> 31) & 1)
+            rs_value |= extension;
+
+        if ((rm_value >> 31) & 1)
+            rm_value |= extension;
+    }
+
+    uint64_t result = rm_value * rs_value;
+
+    if (accumulate && mul_long)
+    {
+        uint64_t rdhi_value = read_register(cpu, rd);
+        uint64_t rdlo_value = read_register(cpu, rn);
+        result += (rdhi_value << 32) | rdlo_value;
+    }
+    else if (accumulate)
+    {
+        result += read_register(cpu, rn);
+    }
+
+    if (mul_long)
+    {
+        write_register(cpu, rd, result >> 32);
+        write_register(cpu, rn, result);
+    }
+    else
+    {
+        write_register(cpu, rd, result);
+    }
 
     if (set_conds)
     {
+        int shift_amt = mul_long ? 63 : 31;
         cpu->cpsr = cpu->cpsr & ~(COND_N_BITMASK | COND_Z_BITMASK);
 
         if (!result)
             cpu->cpsr |= COND_Z_BITMASK;
-        else if (result >> 31)
+        else if (result >> shift_amt)
             cpu->cpsr |= COND_N_BITMASK;
     }
 
-    int array_cycles = get_multiply_array_cycles(rs_value);
+    int array_cycles = get_multiply_array_cycles(rs_value, mul_long, signed_);
 
-    // MUL: 1S + (array_cycles)I, MLA: 1S + (array_cycles + 1)I
-    return 1 + array_cycles + accumulate;
+    //  MUL: 1S + (array_cycles)I,      MLA: 1S + (array_cycles + 1)I
+    // MULL: 1S + (array_cycles + 1)I, MLAL: 1S + (array_cycles + 2)I
+    return 1 + array_cycles + accumulate + 1*mul_long;
 }
 
 /*
